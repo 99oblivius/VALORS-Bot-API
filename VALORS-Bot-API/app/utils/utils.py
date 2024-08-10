82d75f270ea46d1d1865d14a926f635524e8d1d6
99oblivius <99dark.oblivion99@gmail.com>
2024-08-10 23:01:29 +0000

    added resizing image urls

---
+++ VALORS-Bot-API/app/utils/utils.py
@@ -0,0 +1,17 @@
+from urllib.parse import urlparse, urlencode, parse_qs, urlunparse
+
+def resize_image_url(url: str, size: int=64):
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+    query_params['size'] = [str(size)]
+    new_query = urlencode(query_params, doseq=True)
+    new_url = urlunparse((
+        parsed_url.scheme,
+        parsed_url.netloc,
+        parsed_url.path,
+        parsed_url.params,
+        new_query,
+        parsed_url.fragment
+    ))
+    
+    return new_url
