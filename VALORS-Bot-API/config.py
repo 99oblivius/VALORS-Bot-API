10939fa74d3a850a5db76291f852a77d5893e2e4
99oblivius <99dark.oblivion99@gmail.com>
2024-08-10 23:03:05 +0000

    new env variables

--- VALORS-Bot-API/config.py
+++ VALORS-Bot-API/config.py
@@ -10,6 +10,8 @@ class Config:
     REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
     REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
     DATABASE_URL = os.getenv('DATABASE_URL')
+    API_TOKEN = os.getenv('API_TOKEN')
+    DISCORD_GUILD_ID = 1217224187454685295
     DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
     DISCORD_API_ENDPOINT = 'https://discord.com/api/v10'
     DISCORD_CLIENT_ID = os.getenv("DISCORD_CLIENT_ID")
