f04cbcd8532292a5895db1112718928d614d0647
99oblivius <99dark.oblivion99@gmail.com>
2024-09-13 03:56:12 +0000

    fixed autocompletes and other commands causing errors due to no commands being registered

--- VALORS-Bot-API/app/discord/__init__.py
+++ VALORS-Bot-API/app/discord/__init__.py
@@ -14,6 +14,9 @@ class DiscordClient(nextcord.Client):
             log.info(f'Logged in as {self.user.name} (ID: {self.user.id}) at {datetime.now()}')
         else:
             log.warning(f"Warning: Guild with ID {config.DISCORD_GUILD_ID} not found")
+    
+    async def process_application_commands(*args):
+        pass
 
 client = DiscordClient(
     intents=nextcord.Intents.all(), 
